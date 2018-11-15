7478632264ee777ad6a673a89fede6b0d4538efd
Daniel Estevez <daniel.ef@gmail.com>
2018-11-14 19:09:03 -0600

    Initia commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+import (
+  "net/http"
+  "strings"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+  message := r.URL.Path
+  message = strings.TrimPrefix(message, "/")
+  message = "Hello " + message
+  w.Write([]byte(message))
+}
+
+func main() {
+  http.HandleFunc("/", hello)
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    panic(err)
+  }
+}
