2c69d546516d840658c2e9095b54dfbab48c9fb8
Daniel Estevez <daniel.ef@gmail.com>
2018-11-17 23:45:59 -0600

    Testing logging

--- main.go
+++ main.go
@@ -4,7 +4,21 @@ import (
 	"net/http"
 	"strings"
 	"os"
+	"github.com/op/go-logging"
 )
+
+var log = logging.MustGetLogger("example")
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
+func logger() {
+	backend1 := logging.NewLogBackend(os.Stdout, "", 0)
+	backend1Formatter := logging.NewBackendFormatter(backend1, format)
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(logging.INFO, "")	
+	logging.SetBackend(backend1, backend1Formatter)
+}
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Path
@@ -14,6 +28,14 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	logger()
+	log.Debug("debug")
+	log.Info("info")
+	log.Notice("notice")
+	log.Warning("warning")
+	log.Error("err")
+	log.Critical("crit")
+	
 	argsWithProg := os.Args
 	argsWithoutProg := os.Args[1:]
 	
